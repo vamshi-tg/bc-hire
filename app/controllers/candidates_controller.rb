0802b73dbdcd5471327f1ec1314d31cccfe2db9d
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-06-04 14:30:08 +0530

    Will add create user (only half logic)

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,17 @@
 class CandidatesController < ApplicationController
+    def new
+        @candidate = Candidate.new
+    end
+
+    def create
+        @candidate
+        if @candidat.save
+            redirect_to candidates_path
+        else
+            
+        end
+    end
+
     def index
         @candidates = Candidate.all
         @candidates = Candidate.paginate(page: params[:page])
@@ -7,4 +20,22 @@ class CandidatesController < ApplicationController
     def show
         @candidate = Candidate.find(params[:id])
     end
+
+    def edit
+        @candidate = Candidate.find(params[:id])
+    end
+
+    def update
+        @candidate = Candidate.find(params[:id])
+        if @candidate.update_attributes(candidate_params)
+            redirect_to @candidate
+        else
+            render 'edit'
+        end
+    end
+
+    private
+        def candidate_params
+            params.require(:candidate).permit(:name, :email, :experience)
+        end
 end
