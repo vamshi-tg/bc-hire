ca5e943f6d52ee1f53e335df52911db180bd32b5
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-06-04 15:40:09 +0530

    Added home screen haml

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   root "static_pages#home"
   get 'static_pages/home'
+  get 'login', to: "static_pages#login"
 
   resources :candidates
 end
