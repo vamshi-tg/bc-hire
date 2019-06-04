b5a603885b22b384c369be919b0bae7022703fd1
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-06-04 21:40:52 +0530

    Add form for creating new html

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationsController < ApplicationController
+    def new
+        
+    end
+
+    def index
+        
+    end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def current_path_active?(test_path)
+        request.path == test_path ? 'active' : ''
+    end
+
+    # TO-DO How to handle this?
+    def roles
+        ["Full Stack Developer", "Web Developer", "Graphic Designer"]
+    end
 end
