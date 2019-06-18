14054e2ca5f2097cafebd5ffe600413a61bb1d05
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-06-18 10:46:32 +0530

    2-Add new root action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,17 @@
 class SessionsController < ApplicationController
     layout "login", only: [:new]
 
-    before_action :redirect_to_root, only: [:new]
-
     skip_before_action :authenticate
     
     def new
+    end
+
+    def home
+        if logged_in?
+            redirect_to applications_path
+        else
+            redirect_to login_url
+        end
     end
 
     def create
@@ -18,10 +24,5 @@ class SessionsController < ApplicationController
         logout
         redirect_to login_url
     end
-
-    private
-        def redirect_to_root
-            redirect_to root_path if logged_in?
-        end
 end
   
