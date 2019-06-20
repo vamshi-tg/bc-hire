d72fe2b40b260e9b68f78b9443259635f18b0182
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-06-20 16:45:40 +0530

    Add few specs for candidate

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Candidate, type: :model do
   it "is valid with valid attributes" do
     expect(@user1).to be_valid
   end
+
+  it "is not valid without a name" do
+    @user1.name = " "
+    expect(@user1).to_not be_valid
+  end
+
+  it "is not valid without a email" do
+    @user1.email = nil
+    expect(@user1).to_not be_valid
+  end
 end
