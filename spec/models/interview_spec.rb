4beacb99b89e951b5453f243b839fe83c8fa0113
vamshi-tg <vamshi.tg@beautifulcode.in>
2019-07-01 20:13:15 +0530

    Add test case for custom validation

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -41,5 +41,32 @@ RSpec.describe Interview, type: :model do
             interview = FactoryBot.build(:interview, start_time: time, end_time: end_time)
             expect { interview.save }.to raise_error(Exceptions::InvalidTimeSlotException)
         end
+
+    end
+
+    describe "#prepend_scheduled_date_to_time" do
+        context "when schedule_on is present" do
+            it 'should prepend schedule_on to start and end time' do  
+                interview_one.send(:prepend_scheduled_date_to_time)
+                expect(interview_one.start_time.to_date).to eq(interview_one.scheduled_on)
+                expect(interview_one.end_time.to_date).to eq(interview_one.scheduled_on)
+            end
+
+            it "should convert time to IST timezone" do
+                TIME_FORMAT = "%I:%M %p"
+                
+                interview = FactoryBot.build(:interview)
+                start_time_before = interview.start_time.strftime(TIME_FORMAT)
+                end_time_before = interview.end_time.strftime(TIME_FORMAT)
+                
+                interview.send(:prepend_scheduled_date_to_time)
+                
+                start_time_after = interview.start_time.in_time_zone("Kolkata").strftime(TIME_FORMAT)
+                end_time_after = interview.end_time.in_time_zone("Kolkata").strftime(TIME_FORMAT)
+
+                expect(start_time_before).to eq(start_time_after)
+                expect(end_time_before).to eq(end_time_after)
+            end
+        end
     end
 end
